37244486e2713ae81c0a607cbb1cea06d799353a
HarshitkatGIt <116148660+HarshitkatGIt@users.noreply.github.com>
2024-01-30 20:49:28 +0530

    Update Implementation.py

--- Implementation.py
+++ Implementation.py
@@ -19,7 +19,7 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("project.json", scope)
+            flow = InstalledAppFlow.from_client_secrets_file("YourCredentials.json", scope)
             creds = flow.run_local_server(port=0)
 
             with open("token.json", "w") as token:
